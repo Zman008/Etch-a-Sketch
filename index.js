2394524f83edc1da133c8dfc80b3657f165483e3
Sami <zaman.sami8@gmail.com>
2023-05-29 09:36:16 +0600

    add click draw function

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ for (let i = 0; i < 256; i++) {
     div = document.createElement('div');
     grid.appendChild(div);
 }
+
+squares = grid.querySelectorAll('div');
+
+let isMouseDown = false;
+
+document.addEventListener("mousedown", () => {
+    isMouseDown = true;
+});
+
+document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+})
+
+squares.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+        if(isMouseDown)
+            square.style.backgroundColor = 'black';
+    })        
+});
