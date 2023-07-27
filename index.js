ce5f966538949754fc8eed71a75c29086401d0b5
Sami <zaman.sami8@gmail.com>
2023-07-28 03:22:24 +0600

    Adding increase and decrease tiles functionality, also added clear functionality

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const grid = document.querySelector('.grid');
 const clearButton = document.querySelector('.clear');
+const sizePlus = document.querySelector('.size-plus');
+const sizeMinus = document.querySelector('.size-minus');
+const gridSize = document.querySelector('.grid-size');
+let squares;
 let div;
 let isMouseDown = false;
+let size = 16;
 
-for (let i = 0; i < 256; i++) {
-    div = document.createElement('div');
-    grid.appendChild(div);
-}
+createDivs(size * size);
 
-const squares = grid.querySelectorAll('.grid div');
+sizePlus.addEventListener("click", () => {
+    if (size < 100){
+        size += 1;
+        gridSize.textContent = size;
+        createDivs(2 * size - 1);
+    }
+});
+   
+sizeMinus.addEventListener("click", () => {
+    if (size > 8){
+        size -= 1;
+        gridSize.textContent = size;
+        removeDiv(2 * size + 1);
+    }
+});
 
 document.addEventListener("mousedown", () => {
     isMouseDown = true;
@@ -18,15 +34,47 @@ document.addEventListener("mouseup", () => {
     isMouseDown = false;
 });
 
-squares.forEach((square) => {
-    square.addEventListener("mouseenter", () => {
-        if(isMouseDown)
-            square.style.backgroundColor = 'black';
-    });        
-});
-
 clearButton.addEventListener("click", () => {
     squares.forEach((square) => {
-      square.style.backgroundColor = "";  
+        square.style.backgroundColor = "";  
     });
 });
+
+function createDivs(n) {
+    for (let i = 0; i < n; i++) {
+        div = document.createElement('div');
+        grid.appendChild(div);
+    }
+
+    squares = grid.querySelectorAll('.grid div');
+    
+    squares.forEach(div => {
+        div.style.flexBasis = `${100 / size}%`;
+        div.addEventListener('dragstart', (event) => {
+            event.preventDefault();
+        });
+    });
+
+    squares.forEach((square) => {
+        square.addEventListener("mousedown", () => square.style.backgroundColor = 'black');
+        square.addEventListener("mouseenter", () => {
+            if(isMouseDown)
+                square.style.backgroundColor = 'black';
+        });
+    });    
+}
+
+function removeDiv(n) {
+    for (let i = 0; i < n; i++) {
+        grid.removeChild(grid.lastChild);
+    }
+
+    squares = grid.querySelectorAll('.grid div');
+    
+    squares.forEach(div => {
+        div.style.flexBasis = `${100 / size}%`;
+        div.addEventListener('dragstart', (event) => {
+            event.preventDefault();
+        });
+    });
+}
